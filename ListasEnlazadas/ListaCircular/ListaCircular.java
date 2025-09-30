3a3cd03915886286a6a33676f43af2fa6743e509
valentina <valentina.gutierrezc@uqvirtual.edu.co>
2025-09-30 16:08:20 -0500

    feat:creacion de clase ListaCircular y Nodo

--- ListasEnlazadas/ListaCircular/ListaCircular.java
+++ ListasEnlazadas/ListaCircular/ListaCircular.java
@@ -1,4 +1,44 @@
 package co.edu.uniquindio.poo.ListasEnlazadas.ListaCircular;
 
-public class ListaCircular {
+import co.edu.uniquindio.poo.ListasEnlazadas.Nodo;
+
+public class ListaCircular<T extends Comparable<T>> {
+    private Nodo<T> primero;
+    private int tamaño;
+
+    public ListaCircular() {
+        primero=null;
+        tamaño=0;
+    }
+
+    public void agragarPrimero(Nodo<T> newNodo){
+        if (primero == null) {
+            primero = newNodo;
+            newNodo.setProximo(primero);
+        } else {
+            Nodo<T> actual = primero;
+            while (actual.getProximo() != primero) {
+                actual = actual.getProximo();
+            }
+            actual.setProximo(newNodo);    // último apunta al nuevo
+            newNodo.setProximo(primero);   // nuevo apunta al viejo primero
+            primero = newNodo;             // actualizamos primero
+        }
+        tamaño++;
+    }
+
+    public void agregarUltimo(Nodo<T> newNodo){
+        if (primero == null) {
+            primero = newNodo;
+            newNodo.setProximo(primero);
+        } else {
+            Nodo<T> actual = primero;
+            while (actual.getProximo() != primero) {
+                actual = actual.getProximo();
+            }
+            actual.setProximo(newNodo);
+            newNodo.setProximo(primero);
+        }
+        tamaño++;
+    }
 }
