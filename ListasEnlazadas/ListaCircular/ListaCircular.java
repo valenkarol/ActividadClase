b64f19921768348269bb6361f0e7acf1f43d598e
Karol <karolm.davilas@uqvirtual.edu.co>
2025-10-02 18:21:58 -0500

    feat:metodo agregarOrdenado en ListaCircular.java

--- ListasEnlazadas/ListaCircular/ListaCircular.java
+++ ListasEnlazadas/ListaCircular/ListaCircular.java
@@ -11,7 +11,7 @@ public class ListaCircular<T extends Comparable<T>> {
         tamaño=0;
     }
 
-    public void agragarPrimero(Nodo<T> newNodo){
+    public void agregarPrimero(Nodo<T> newNodo){
         if (primero == null) {
             primero = newNodo;
             newNodo.setProximo(primero);
@@ -40,5 +40,22 @@ public class ListaCircular<T extends Comparable<T>> {
             newNodo.setProximo(primero);
         }
         tamaño++;
+    }
+    // nsertar ordenado
+    public void agregarOrdenado(T dato) {
+        Nodo<T> nuevo = new Nodo<>(dato);
+
+        if (primero == null || dato.compareTo(primero.getDato()) <= 0) {
+            agregarPrimero(nuevo);
+        } else {
+            Nodo<T> actual = primero;
+            while (actual.getProximo() != primero &&
+                    dato.compareTo(actual.getProximo().getDato()) > 0) {
+                actual = actual.getProximo();
+            }
+            nuevo.setProximo(actual.getProximo());
+            actual.setProximo(nuevo);
+            tamaño++;
+        }
     }
 }
